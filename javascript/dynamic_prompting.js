69ae030292fcfaecde53851a8a5fc83d96f4bda6
TheAwesomeGoat <38667368+TheAwesomeGoat@users.noreply.github.com>
2022-10-25 12:41:11 +0200

    folders for organization

---
+++ javascript/dynamic_prompting.js
@@ -0,0 +1,18 @@
+onUiUpdate(function(){
+  check_collapsibles();
+})
+
+function check_collapsibles() {
+  var coll = gradioApp().querySelectorAll(".collapsible")
+  for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
