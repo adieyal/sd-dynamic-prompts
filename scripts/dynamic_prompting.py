7cfd16eb26e789456e751f1078df139e4b624264
Adi Eyal <adi@clearforest.io>
2022-10-31 08:07:35 +0200

    Updated style.css to fix light and dark themes

--- scripts/dynamic_prompting.py
+++ scripts/dynamic_prompting.py
@@ -19,7 +19,7 @@ logger.setLevel(logging.INFO)
 base_dir = Path(scripts.basedir())
 
 WILDCARD_DIR = getattr(opts, "wildcard_dir", base_dir / "wildcards")
-VERSION = "0.13.4"
+VERSION = "0.13.5"
 
 
 wildcard_manager = WildcardManager(WILDCARD_DIR)
