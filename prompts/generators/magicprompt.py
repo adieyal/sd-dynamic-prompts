d0b337149c078a707f4086544f2f5806b8fb50f8
Brendan Byrd <GitHub@ResonatorSoft.org>
2022-11-09 12:56:00 -0500

    Add a cleanup function for Magic Prompts
    The model was created before there was more uniformity with how tags and
    weights were specified, and the more creative prompts can go off in weird
    directions with the symbolism.  We can clean this up pretty well with the
    right regular expressions, though.

--- prompts/generators/magicprompt.py
+++ prompts/generators/magicprompt.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from . import PromptGenerator
 
+import re
 from tqdm import trange
 
 class MagicPromptGenerator(PromptGenerator):
@@ -47,7 +48,28 @@ class MagicPromptGenerator(PromptGenerator):
         for i in trange(len(prompts), desc='Generating Magic prompts'):
             prompt       = prompts[i]
             magic_prompt = self._generator(prompt, max_length=self._max_prompt_length, temperature=self._temperature)[0]["generated_text"]
+            magic_prompt = self.clean_up_magic_prompt(magic_prompt)
             new_prompts.append(magic_prompt)
 
         return new_prompts
 
+    def clean_up_magic_prompt(self, prompt):
+        prompt = prompt.translate( str.maketrans('{}', '()') ).strip()
+
+        prompt = re.sub(r'^\W+|\W+$', '',         prompt)  # useless non-word characters at the begin/end
+        prompt = re.sub(r'\(\s+', '(',            prompt)  # clean up whitespace in weighted parens
+        prompt = re.sub(r'\s+\)', ')',            prompt)
+        prompt = re.sub(r'\b\s+\-\s+\b', '-',     prompt)  # clean up whitespace in hyphens between words
+        prompt = re.sub(r'\s*[,;|:\.]+\s*', ', ', prompt)  # other analogues to ', '
+        prompt = re.sub(r'\s+_+\s+', ' ',         prompt)  # useless underscores between phrases
+        prompt = re.sub(r'\b,\s*,\s*\b', ', ',    prompt)  # empty phrases
+
+        # Translate bangs into proper weight modifiers
+        for match in re.findall(r'\b([\w\s\-]+)(\!+)', prompt):
+            phrase     = match[0]
+            full_match = match[0] + match[1]
+            weight     = round( pow(1.1, len(match[1])), 2 )
+
+            prompt = prompt.replace(full_match, f'({phrase}:{weight})')
+
+        return prompt
