4d66a5e8bf024fd779f761ea4cd8dcb985a63b84
Adi Eyal <adi@clearforest.io>
2023-01-18 18:24:54 +0200

    Using a dummy attention grabber if spacy isn't installed

--- prompts/generator_builder.py
+++ prompts/generator_builder.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import List
 import logging
 
 from dynamicprompts.generators import (
@@ -11,8 +12,23 @@ from dynamicprompts.generators import (
     JinjaGenerator
 )
 
+logger = logging.getLogger(__name__)
+
+class DummyAttentionGenerator:
+    def __init__(self, generator: PromptGenerator, *args, **kwargs):
+        self._generator = generator
+        logger.warning("Using the DummyAttentionGenerator, this is possibly because you don't have spacy installed. Install it with `python -m pip install dynamicprompts[attentiongrabber]`")
+
+    def generate(self, *args, **kwargs) -> List[str]:
+        return self._generator.generate(*args, **kwargs)
+
 from dynamicprompts.generators.magicprompt import MagicPromptGenerator
-from dynamicprompts.generators.attentiongenerator import AttentionGenerator
+
+try:
+    from dynamicprompts.generators.attentiongenerator import AttentionGenerator
+    raise NameError()
+except NameError:
+    AttentionGenerator = DummyAttentionGenerator
 
 logger = logging.getLogger(__name__)
 
