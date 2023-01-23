fc614a730c86a7f190c4d5a5363e313714d0e663
Adi Eyal <adi@clearforest.io>
2023-01-23 15:09:01 +0200

    Slight ui improvements to the noodle soup import script

--- _tools/import_noodle_soup_prompts.py
+++ _tools/import_noodle_soup_prompts.py
@@ -9,7 +9,10 @@ You would then import these via the WebUI to your wildcards collection.
 """
 import collections
 import requests
-import os
+from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_tag_group(tag):
@@ -28,17 +31,47 @@ def get_grouped_tags():
 
 
 def main():
-    grouped_tags = get_grouped_tags()
+    count_files = 0
+    current_path = Path.cwd()
+    if current_path.name == "_tools":
+        current_path = current_path.parent
 
-    for tag_group_name, tags_in_group in sorted(grouped_tags.items()):
-        tag_group_name = "nsp" if len(tags_in_group) == 1 else f"nsp-{tag_group_name}"
-        for tag, entries in sorted(tags_in_group):
-            filename = f"./collections/nsp/{tag_group_name}/{tag}.txt"
-            os.makedirs(os.path.dirname(filename), exist_ok=True)
-            with open(filename, "w", encoding="utf-8") as f:
-                for entry in sorted(entries):
-                    f.write(f"{entry}\n")
-            print(f"{filename}: {len(entries)} entries")
+    can_overwrite = None
+
+    grouped_tags = get_grouped_tags()
+    collections_dir = current_path / "collections"
+
+    if not collections_dir.exists():
+        print("Could not find the collections directory. You should run this from the root of the repository")
+    else:
+        for tag_group_name, tags_in_group in sorted(grouped_tags.items()):
+            tag_group_name = "nsp" if len(tags_in_group) == 1 else f"nsp-{tag_group_name}"
+            for tag, entries in sorted(tags_in_group):
+                filename = collections_dir / f"./nsp/{tag_group_name}/{tag}.txt"
+                filename.parent.mkdir(parents=True, exist_ok=True)
+
+                if can_overwrite is None and filename.exists():
+                    answer = input(f"Skipping {filename} as it already exists. Should we overwrite existing files? (y/n)")
+                    if answer.strip().lower() == "y":
+                        can_overwrite = True
+                    else:
+                        can_overwrite = False
+                    
+                if can_overwrite is None or can_overwrite == True:
+                    count_files += 1
+                    with filename.open("w", encoding="utf-8") as f:
+                        for entry in sorted(entries):
+                            try:
+                                f.write(f"{entry}\n")
+                            except UnicodeEncodeError:
+                                logger.warning(f"Error writing {entry} to {filename}")
+
+                    print(f"{filename}: {len(entries)} entries")
+
+        print("")
+        print(f"{count_files} files copied to {collections_dir}")
+        if count_files > 0:
+            print("You should now import these via the WebUI to your wildcards collection using the Wildcards Manager tab.")
 
 
 if __name__ == "__main__":
