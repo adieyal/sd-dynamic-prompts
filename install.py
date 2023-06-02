2dd5501b4ef98b27b3d1963d358540b1b8f12f29
Aarni Koskela <akx@iki.fi>
2023-06-02 21:44:27 +0300

    install.py: try to patch sys.path if the extension is not there? (#496)
    Fixes #486

--- install.py
+++ install.py
@@ -1,4 +1,14 @@
 if __name__ == "__main__":
-    from sd_dynamic_prompts.version_tools import install_requirements
+    try:
+        from sd_dynamic_prompts.version_tools import install_requirements
+    except ImportError:
+        # This patching shouldn't be necessary, but who knows... See issue #486.
+        import os
+        import sys
+
+        extension_dir = os.path.dirname(os.path.abspath(__file__))
+        if extension_dir not in sys.path:
+            sys.path.insert(0, extension_dir)
+        from sd_dynamic_prompts.version_tools import install_requirements
 
     install_requirements()
